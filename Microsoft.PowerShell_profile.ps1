c8d65c626bb6b77244c5a6bb179288a22835ae94
Miguel Pérez Regueiro <67692547+MiguelRegueiro@users.noreply.github.com>
2025-04-27 15:09:04 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,11 @@
 Invoke-Expression (&starship init powershell)
+# Enable predictive suggestions (like zsh-autosuggestions)
+# Set more aggressive prediction settings
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#7a7a7a' }
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -MaximumHistoryCount 10000
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+fastfetch
